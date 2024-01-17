031bf5af2247211740af79c70c232d83078196f8
Panchoneit0r <perromendigo321@gmail.com>
2024-01-16 23:10:20 -0600

    first commit

---
+++ Source/MenuOnlie/MenuOnlieGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MenuOnlieGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMenuOnlieGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMenuOnlieGameMode();
+};
+
+
+
